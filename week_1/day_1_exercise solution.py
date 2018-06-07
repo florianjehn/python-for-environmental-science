c95076a2afc49709eec566857658055b8d6ee73e
gh2668 <florian.u.jehn@umwelt.uni-giessen.de>
2018-06-07 16:16:42 +0200

    Finished first version of exercise for day one

---
+++ week_1/day_1_exercise solution.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Jun 07 16:11 2018
+@author(s): Florian U. Jehn
+"""
+
+print("Hello there")
+name = input("What is your name?")
+print("Hello " + name)
+print("Your name has " + str(len(name)) + " letters")
+height = float(input("What is your height?"))
+height *= 25
+print("Your height times 25 is " + str(height))
