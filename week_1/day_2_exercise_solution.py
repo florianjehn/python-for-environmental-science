08a99ce90a82ce26a39e45c6bcf84e4cef10a457
gh2668 <florian.u.jehn@umwelt.uni-giessen.de>
2018-06-08 13:42:06 +0200

    Added solution for first exercise

--- week_1/day_2_exercise_solution.py
+++ week_1/day_2_exercise_solution.py
@@ -3,3 +3,12 @@
 Created on Jun 08 10:11 2018
 @author(s): Florian U. Jehn
 """
+# Exercise 1 (Conditionals)
+word = input("Word please!")
+if len(word) > 4:
+    print(word)
+    if len(word) == 5:
+        print("You choose a fine word, sir!")
+print(len(word))
+
+    
