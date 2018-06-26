1ad843184686edc4022abffc25fdeaa998afce65
gh2668 <florian.u.jehn@umwelt.uni-giessen.de>
2018-06-26 14:35:20 +0200

    Finished exercise solutions

--- week_1/day_2_exercise_solution.py
+++ week_1/day_2_exercise_solution.py
@@ -11,4 +11,63 @@ if len(word) > 4:
         print("You choose a fine word, sir!")
 print(len(word))
 
+# Exercise 2
+for i in range(1,11):
+    print(i)
+    
+# Exercise 3
+import time
+seconds = time.time()
+minutes = seconds / 60
+print(str(minutes) +" minutes have passed since epoch")
+
+# Exercise 4
+s1 = 10
+s2 = 5 
+s3 = 100
+
+if s1 > s2 + s3:
+    print("Does not work")
+elif s2 > s1 + s3:
+    print("Does not work")
+elif s3 > s1 + s2:
+    print("Does not work")
+else:
+    print("Perfectly good triangle")
+    
+# Exercise 5
+for i in range(1,8):
+    print(str(i) * i)    
+    
+# Exercise 6
+month = "jan"
+if month == "jan":
+    print(31)
+elif month == "feb":
+    print(28)
+elif month == "mar":
+    print (31)
+elif month == "apr":
+    print(30)
+elif month == "may":
+    print(31)
+elif month == "jun":
+    print(30)
+elif month == "jul":
+    print(31)
+elif month == "aug":
+    print(31)
+elif month == "sep":
+    print(30)
+elif month == "oct":
+    print(31)
+elif month == "nov":
+    print(30)
+elif month == "dec":
+    print(31)
+else:
+    print("you gave me no month")
+
+
+
     
