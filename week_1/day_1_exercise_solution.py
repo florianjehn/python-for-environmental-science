879909741df6cd26de19e573183823c4616fdc1d
gh2668 <florian.u.jehn@umwelt.uni-giessen.de>
2018-06-26 14:09:18 +0200

    Updated exercises and solutions

--- week_1/day_1_exercise_solution.py
+++ week_1/day_1_exercise_solution.py
@@ -3,7 +3,7 @@
 Created on Jun 07 16:11 2018
 @author(s): Florian U. Jehn
 """
-
+# Exercise 1
 print("Hello there")
 name = input("What is your name?")
 print("Hello " + name)
@@ -11,3 +11,23 @@ print("Your name has " + str(len(name)) + " letters")
 height = float(input("What is your height?"))
 height *= 25
 print("Your height times 25 is " + str(height))
+
+# Exercise 2
+side = "*       *"
+print(side)
+print(side)
+print(side)
+print(side)
+print(side)
+print(side)
+bottom = "* * * * *"
+print(bottom)
+
+# Exercise 3
+word = input("Give me a word!")
+print(word[0])
+
+# Exercise 4
+celsius = 40
+fahrenheit = 1.8 * celsius + 32
+print(fahrenheit)
