482971f8dcf2c62569d48ed58307101c97dcad07
gh2668 <florian.u.jehn@umwelt.uni-giessen.de>
2018-07-04 15:19:18 +0200

    Continued on solutions

--- week_2/day_5_exercise_solution.py
+++ week_2/day_5_exercise_solution.py
@@ -6,6 +6,7 @@ Created on Wed Jul  4 09:39:55 2018
 """
 import datetime
 import time
+import dateutil
 
 
 # Exercise 1
@@ -51,21 +52,56 @@ print(transform("This is a sentence!"))
 def print_n_pause():
     """Prints hello 10 seconds longs and makes pauses in between"""
     start = datetime.datetime.now()
-    while (datetime.datetime.now() - start).seconds < 1:
+    while (datetime.datetime.now() - start).seconds < 10:
         print("Hello")
         time.sleep(0.7)
         
-print_n_pause()
+# print_n_pause()
 
 
 # Exercise 5
 def bday_timer(bday):
     """Tells how old you are and how long it is till your next birthday."""
-    age = (datetime.datetime.now()- bday).years
+    # Calculate the age
+    today = datetime.datetime.today()
+    age = dateutil.relativedelta.relativedelta(today, bday)
+    age = age.years
     print("Your age is {} years".format(age))
+    # Calculate the time till the next birthday
+    # Normalize the birthday to get a timedelta
+    bday_changed_year = datetime.datetime(today.year, bday.month, bday.day)
+    # Calculate the difference to the birthday
+    till_bday = today - bday_changed_year
+    # Check if the the bday already was this year
+    if till_bday.days < 0:
+        print("Only {} days until your birthday".format(- till_bday.days))
+    else:
+        bday_changed_year = datetime.datetime(bday_changed_year.year + 1, 
+                                              bday_changed_year.month, 
+                                              bday_changed_year.day)
+        till_bday = bday_changed_year - today
+        print("Only {} days until your next birthday".format(till_bday.days))
     
 bday = datetime.datetime(1989, 12, 23)
 bday_timer(bday)
+
+
+# Exercise 6
+def double_day(bday1, bday2):
+    """Calculates the day when one person is double as old as the other one"""
+    if bday1 > bday2:
+        delta = bday1 - bday2
+        double_day = bday1 + delta
+    else:
+        delta = bday2 - bday1
+        double_day = bday2 + delta
+    return double_day
+
+bday1 = datetime.date(1990, 8, 31)
+bday2 = datetime.date(1989, 12, 23)
+double_day = double_day(bday1, bday2)
+print("The double day of {} and {} is {}".format(bday1, bday2, double_day))
+    
 
 
 
